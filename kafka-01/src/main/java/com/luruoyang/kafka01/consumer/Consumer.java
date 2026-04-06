1d317ee5528a77d27a9a9c7d6a96aeaa75f7f036
luruoyang8241 <ruoyang8241@126.com>
2026-04-06 22:51:55 +0800

    入门实战

--- kafka-01/src/main/java/com/luruoyang/kafka01/consumer/Consumer.java
+++ kafka-01/src/main/java/com/luruoyang/kafka01/consumer/Consumer.java
@@ -1,4 +1,22 @@
 package com.luruoyang.kafka01.consumer;
 
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class Consumer {
+
+    @KafkaListener(topics = {"topic1"})
+    public void onMessage1(String message) {
+//    public void onMessage1(ConsumerRecord<String, Object> record) {
+//        log.warn("消费者收到消息：topic:{}, partition: {}, value: {}",
+//                record.topic(), record.partition(), record.value());
+        log.warn("消费者接收到消息：{}", message);
+
+    }
+
 }
--- kafka-01/src/main/java/com/luruoyang/kafka01/controller/ProducerController.java
+++ kafka-01/src/main/java/com/luruoyang/kafka01/controller/ProducerController.java
@@ -1,16 +1,24 @@
 package com.luruoyang.kafka01.controller;
 
-import jakarta.annotation.Resource;
+//import jakarta.annotation.Resource;
+
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.lang.Nullable;
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.util.concurrent.SuccessCallback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
 
@@ -19,7 +27,7 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class ProducerController {
 
-    @Resource
+    @javax.annotation.Resource
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     @GetMapping("/{msg}")
@@ -31,10 +39,34 @@ public class ProducerController {
 
     @GetMapping("/callback1/{msg}")
     public void sendMsg2(@PathVariable("msg") String msg) {
+        kafkaTemplate.send("topic1", "666", msg).
+                addCallback(result -> {
+                    RecordMetadata metadata = result.getRecordMetadata();
+                    String topic = metadata.topic();
+                    int partition = metadata.partition();
+                    long offset = metadata.offset();
+                    log.warn("消息发送成功：topic: {}, partition: {}, offset: {}", topic, partition, offset);
+                }, ex -> {
+                    log.warn("消息发送失败：{}", ex.getMessage());
+                });
     }
 
     @GetMapping("/callback2/{msg}")
     public void sendMsg3(@PathVariable("msg") String msg) {
+        kafkaTemplate.send("topic1", "555", msg)
+                .addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+                    @Override
+                    public void onFailure(Throwable ex) {
+                        log.warn("消息发送失败：{}", ex.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(@Nullable SendResult<String, Object> result) {
+                        RecordMetadata metadata = result.getRecordMetadata();
+                        log.warn("消息发送成功：topic: {}, partition: {}, offset: {}",
+                                metadata.topic(), metadata.partition(), metadata.offset());
+                    }
+                });
     }
 
 }
